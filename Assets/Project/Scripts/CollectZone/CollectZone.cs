9e735d4e2dd137ecaac7233d1ef017ab974739f5
CheshireCat744 <mrmainera@gmail.com>
2025-04-09 21:35:04 +0700

    update first scene

--- Assets/Project/Scripts/CollectZone/CollectZone.cs
+++ Assets/Project/Scripts/CollectZone/CollectZone.cs
@@ -1,11 +1,14 @@
+using Project.EntenEller.Base.Scripts.Advanced.Notifiers;
 using Project.EntenEller.Base.Scripts.Cache.Components.Master;
 
 namespace Project.Scripts.CollectZone
 {
     public class CollectZone : EEBehaviour
     {
+        public EENotifier DropItemNotifier;
         public void OnPlayerEnter(Inventory.Base.Inventory inventory)
         {
+            DropItemNotifier.Notify();
             inventory.AdderToCrossScene.AddAllItemToCrossScene(true);
         }
     }
